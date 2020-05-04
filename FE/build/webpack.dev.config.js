7224c7ca6e1e3705f2cc082a0dc18b62a38a3725
Bin <1510120026@vip.henu.edu.cn>
2020-05-04 11:33:48 +0800

    修复部署脚本的bug

--- FE/build/webpack.dev.config.js
+++ FE/build/webpack.dev.config.js
@@ -16,6 +16,7 @@ module.exports = Object.assign(baseConfig, {
                 pathRewrite: {
                     '^/proxy': ''
                 }
+            }
         }
     },
     plugins: (baseConfig.plugins || []).concat([
